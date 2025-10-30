612f241efe3a502451b37557cdd9181ffcb60d70
Thomas Childs <thomas.c.childs@gmail.com>
2025-10-30 14:28:03 +0000

    installed dependencies

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,8 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script lang="ts">
+  import './app.css';
+  import svelteLogo from './assets/svelte.svg';
+  import viteLogo from '/vite.svg';
+  import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
